b5f3c70280717e8fde486c81a2a4865567c4823c
Patrick Teixeira <tp1002251@gmail.com>
2023-03-03 19:32:40 -0300

    Terminando as config da API

--- vite-project/src/pages/DashB.tsx
+++ vite-project/src/pages/DashB.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-const DashB = () => {
-  return (
-    <div>DashB</div>
-  )
+interface WeatherData {
+  main: {
+    temp: number;
+  };
+  sys: {
+    country: string;
+  };
+}
+interface User {
+  name?: string;
+  email?: string;
+  lastName?: string;
+  city?: string;
+  country?: string;
+  password?: string;
 }
 
-export default DashB
+
+const usersStorage = JSON.parse(localStorage.getItem("users_bd") || "null") as User[] | null;
+const city: string = usersStorage ? usersStorage[0]["city"] || "" : "";
+
+const DashB = () => {
+  const [dadosTemp, setDadosTemp] = useState<WeatherData>();
+
+  useEffect(() => {
+    async function fetchDados() {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+        city
+      )}&units=metric&appid=6c3ad536f4936801eb863a79db679f51`;
+      const response = await fetch(url);
+      const json = await response.json();
+      setDadosTemp(json);
+    }
+    fetchDados();
+  }, []);
+
+  return (
+    <div>
+      {dadosTemp && (
+        <div>
+          <p> {city}: {dadosTemp.main.temp}°C</p>
+          <p>Pais: {dadosTemp.sys.country}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DashB;
