ace945f8c4257ae199bf27f343d18326b44616a0
Patrick Teixeira <tp1002251@gmail.com>
2023-03-06 11:33:02 -0300

    input e os botoes prontos com a estilização

--- vite-project/src/pages/DashB.tsx
+++ vite-project/src/pages/DashB.tsx
@@ -23,11 +23,34 @@ interface User {
   country?: string;
   password?: string;
 }
+interface Task {
+  id: number;
+  h: string | null;
+  t: string | null;
+}
+interface Button {
+  value: string;
+}
+
+interface Style {
+  boxShadow: string;
+  width: string;
+}
+
 
 const DashB = () => {
+  const [chenge, setChange] = useState<string>('');
   const [dadosTemp, setDadosTemp] = useState<WeatherData>();
   const [horaAtual, setHoraAtual] = useState<string>('');
+  const [activeButton, setActiveButton] = useState<Button>();
   const [dayAtual,setDayAtual] = useState<string>('');
+  const [listMonday, setListMonday] = useState<Task[]>([]);
+  const [listTuesday, setListTuesday] = useState<Task[]>([]);
+  const [listWednesday, setListWednesday] = useState<Task[]>([]);
+  const [listThursday, setListThursday] = useState<Task[]>([]);
+  const [listFriday, setListFriday] = useState<Task[]>([]);
+  const [listSaturday, setListSaturday] = useState<Task[]>([]);
+  const [listSunday, setListSunday] = useState<Task[]>([]);
   const usersStorage = JSON.parse(localStorage.getItem("users_bd") || "null") as User[] | null;
   const city: string = usersStorage ? usersStorage[0]["city"] || "" : "";
 
@@ -71,6 +94,126 @@ const DashB = () => {
   }, []);
 
 
+  // Conteudo principal 
+  // 1- Var change control
+  const handleChange: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
+    setChange(event.target.value);
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const pHor = formData.get('horario');
+    const task = formData.get('tesk');
+    addNewUser(pHor as string, task as string);
+    e.currentTarget.reset();
+  };
+  
+  const addNewUser = (horario: string | null, tesk: string | null) => {
+    if (!horario || !tesk) {
+      return alert('Por favor, preencha todos os campos');
+    }
+    const newUser = { id: Date.now(), h: horario, t: tesk };
+    let listToUpdate: Task[] = [];
+
+    switch (chenge) {
+      case "Monday":
+        listToUpdate = listMonday;
+        break;
+      case "Tuesday":
+        listToUpdate = listTuesday;
+        break;
+      case "Wednesday":
+        listToUpdate = listWednesday;
+        break;
+      case "Thursday":
+        listToUpdate = listThursday;
+        break;
+      case "Friday":
+        listToUpdate = listFriday;
+        break;
+      case "Saturday":
+        listToUpdate = listSaturday;
+        break;
+      case "Sunday":
+        listToUpdate = listSunday;
+        break;
+      default:
+        break;
+    }
+    if (listToUpdate.some((user) => user.h === horario)) {
+      return alert('Horário já ocupado');
+    }
+    const updatedList = [...listToUpdate, newUser];
+    switch (chenge) {
+      case "Monday":
+        setListMonday(updatedList);
+        break;
+      case "Tuesday":
+        setListTuesday(updatedList);
+        break;
+      case "Wednesday":
+        setListWednesday(updatedList);
+        break;
+      case "Thursday":
+        setListThursday(updatedList);
+        break;
+      case "Friday":
+        setListFriday(updatedList);
+        break;
+      case "Saturday":
+        setListSaturday(updatedList);
+        break;
+      case "Sunday":
+        setListSunday(updatedList);
+        break;
+      default:
+        break;
+    }
+  };
+  
+  const clearList = (day: string) => {
+    switch (day) {
+      case "Monday":
+        setListMonday([]);
+        break;
+      case "Tuesday":
+        setListTuesday([]);
+        break;
+      case "Wednesday":
+        setListWednesday([]);
+        break;
+      case "Thursday":
+        setListThursday([]);
+        break;
+      case "Friday":
+        setListFriday([]);
+        break;
+      case "Saturday":
+        setListSaturday([]);
+        break;
+      case "Sunday":
+        setListSunday([]);
+        break;
+      default:
+        break;
+    }
+  }
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const clickedButton = event.target as HTMLButtonElement;
+    setActiveButton({ value: clickedButton.value });
+    setChange(clickedButton.value);
+  };
+  
+  const getButtonStyle = (buttonValue: string): Style => {
+    if (activeButton && activeButton.value === buttonValue) {
+      return {
+        boxShadow: '0 2px 2px rgba(0, 0, 0, 0.3)',
+        width: '290px'
+      };
+    }
+    return {} as Style;
+  };
+
   return (
     <div className="divPrinc">
       <header className="headerTop">
@@ -101,6 +244,53 @@ const DashB = () => {
           </Link>          
         </div>
       </header>
+      <main>
+        <section className="container-form">
+        <form onSubmit={handleSubmit} className="flex-form">
+            <input type="text" name="tesk" className="input-tesk" placeholder="Task or issue" maxLength={35} required></input>
+            <select id="daysOfWeek" onChange={handleChange} className="input-sel" defaultValue="">
+              <option value="Monday">Monday</option>
+              <option value="Tuesday">Tuesday</option>
+              <option value="Wednesday">Wednesday</option>
+              <option value="Thursday">Thursday</option>
+              <option value="Friday">Friday</option>
+              <option value="Saturday">Saturday</option>
+              <option value="Sunday">Sunday</option>
+            </select>
+              <input type="text" name="horario" className="input-hora" placeholder="01h 32m" required></input>
+            <div className="class-btns">
+              <button type="submit" className="btn-add">+ Add to calendar</button>
+              <button type="button" className="btn-del" onClick={() => clearList(chenge)}>- Delet all</button>
+            </div>
+          </form>
+        </section>
+        <section className="days-week">
+            <button value="Monday" onClick={handleClick} style={{ ...getButtonStyle('Monday'), backgroundColor: 'red' }}>
+              Monday
+            </button>
+            <button value="Tuesday" onClick={handleClick} style={{...getButtonStyle('Tuesday'), backgroundColor: 'rgba(255, 128, 0, 1)'}}>
+              Tuesday
+            </button>
+            <button value="Wednesday" onClick={handleClick} style={{...getButtonStyle('Wednesday'), backgroundColor: 'rgba(255, 206, 0, 1)' }}>
+              Wednesday
+            </button>
+            <button value="Thursday" onClick={handleClick} style={{...getButtonStyle('Thursday'), backgroundColor: 'rgba(255, 0, 36, 0.7)' }}>
+              Thursday
+            </button>
+            <button value="Friday" onClick={handleClick} style={{...getButtonStyle('Friday'), backgroundColor: 'rgba(255, 128, 0, 0.7)' }}>
+              Friday
+            </button>
+            <button value="Saturday" onClick={handleClick} style={{...getButtonStyle('Saturday'), backgroundColor: 'rgba(255, 206, 0, 0.7)' }}>
+              Saturday
+            </button>
+            <button value="Sunday" onClick={handleClick} style={{...getButtonStyle('Sunday'), backgroundColor: 'rgba(255, 0, 36, 0.5)' }}>
+              Sunday
+            </button>
+        </section>
+        <section>
+          <p className="caixa" style={{backgroundColor:'rgba(255, 255, 255, 1)'}}>Time</p>
+        </section>
+      </main>
     </div>
   );
 };
